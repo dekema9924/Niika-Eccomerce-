42935ee1c1bf6f18d5b68a88ff31d2aad7dcbdac
dekema9924 <danekema9924@gmail.com>
2026-02-02 20:48:11 -0800

    added all product section

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,24 @@
 
 
-import React from 'react'
-
+import Products from "@/components/layout/home/Products"
+import Link from "next/link"
+import { fakeProductsData } from "../helpers/fakeProductData"
+import TrustSection from "@/components/layout/home/TrustSection"
 export default function AllProductspage() {
     return (
-        <div>
+        <>
+            <div className="p-4 sm:p-7 max-w-7xl mx-auto">
+                {/* Breadcrumb navigation */}
+                <div className="flex gap-2 text-xs sm:text-sm text-gray-500 mb-6 sm:mb-8">
+                    <Link href={'/'} className="hover:text-gray-700">Home</Link>
+                    <span>/</span>
+                    <span className="text-black">All Products</span>
+                </div>
 
-        </div>
+                <Products heading="All Products" products={fakeProductsData} />
+            </div>
+            <TrustSection />
+
+        </>
     )
 }
